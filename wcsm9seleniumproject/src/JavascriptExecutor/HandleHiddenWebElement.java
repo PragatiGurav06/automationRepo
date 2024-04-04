2d8e4647265e8174bac82d6db2047e7f14072fbc
abhil <abhil@192.168.1.7>
2024-04-04 10:54:30 +0530

    hiddenweb element

--- wcsm9seleniumproject/src/JavascriptExecutor/HandleHiddenWebElement.java
+++ wcsm9seleniumproject/src/JavascriptExecutor/HandleHiddenWebElement.java
@@ -2,7 +2,10 @@ package JavascriptExecutor;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandleHiddenWebElement {
@@ -12,8 +15,11 @@ public class HandleHiddenWebElement {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		//navigate to demo app
-		driver.get("https://www.facebook.com/");
+		driver.get("https://www.facebook.com/signup");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		WebElement optional = driver.findElement(By.name("custom_gender"));
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("arguments[0].value='Transgender'",optional);
 
 	} 
 
