d8fac440d5fb0b17cf8429012e910aef1c9dd251
abhil <abhil@192.168.1.7>
2024-04-07 09:44:10 +0530

    Window Handling

---
+++ wcsm9seleniumproject/src/WindowHandling/OPenNewTab.java
@@ -0,0 +1,22 @@
+package WindowHandling;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OPenNewTab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		//navigate to zomato app
+		driver.get("https://www.zomato.com/india");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.switchTo().newWindow(WindowType.TAB);
+
+	}
+
+}
